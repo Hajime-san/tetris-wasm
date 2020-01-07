5f66d216cf3592a9c97a2bead38dc7d3361e8830
Hajime-san <utd.c.r.d.s.a@gmail.com>
2020-01-07 22:37:41 +0900

    migrate function module

---
+++ crate/src/data.rs
@@ -0,0 +1,19 @@
+pub struct Number {}
+
+impl Number {
+    pub const ROW: i32 = 10;
+    pub const COLUMN: i32 = 16;
+    pub const A: i32 = 4;
+    pub const DOWN_KEY: i32 = 40;
+    pub const HARD_DOWN_KEY: i32 = 90;
+    pub const UP_KEY: i32 = 38;
+    pub const LEFT_KEY: i32 = 37;
+    pub const RIGHT_KEY: i32 = 39;
+    pub const ENTER_KEY: i32 = 13;
+    pub const PAUSE_KEY: i32 = 80;
+    pub const LEFT_MOVE: i32 = -1;
+    pub const RIGHT_MOVE: i32 = 1;
+    pub const DEGREES: i32 = 90;
+    pub const QUEUE_ROW: i32 = 6;
+    pub const QUEUE_COLUMN: i32 = 8;
+}
