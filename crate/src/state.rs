be369dcfc3e4f8499bb694b515fcb728a8715435
Hajime-san <utd.c.r.d.s.a@gmail.com>
2020-01-14 23:09:37 +0900

    initialize trait

--- crate/src/state.rs
+++ crate/src/state.rs
@@ -6,20 +6,19 @@ use crate::store;
 pub struct Movable {}
 
 impl Movable {
-    pub fn left(
-        field: &store::dynamics::FIELD,
-        current_block: &store::statics::BlockPosition,
-    ) -> bool {
+    pub fn left(field: &Vec<i32>, current_block: &store::statics::BlockPosition) -> bool {
         let mut flag = false;
+
+        // check fixed block
         for (i, v) in field.iter().enumerate() {
             for n in current_block {
                 if i == *n as usize {
-                    println!("{}", v);
                     flag = true;
                 }
             }
         }
 
+        // check side wall
         for v in current_block {
             if func::fix_digit(*v) == func::fix_digit(store::statics::Number::ROW) {
                 println!("{} is left side", *v);
--- crate/src/store/dynamics.rs
+++ crate/src/store/dynamics.rs
@@ -1,38 +1,133 @@
+use math::round;
+use rand::seq::SliceRandom;
 use rand::{thread_rng, Rng};
+use serde::{Deserialize, Serialize};
+use serde_json::Result;
 use std::convert::TryInto;
 
 use crate::store;
 
-pub type FIELD = Vec<i32>;
-
-#[derive(Debug, Default)]
-pub struct Field(FIELD);
-
 pub const FIELD_LENGTH: i32 = store::statics::Number::ROW * store::statics::Number::COLUMN;
 
-impl Field {
-    pub fn init() -> FIELD {
-        let mut a = Vec::with_capacity(FIELD_LENGTH.try_into().unwrap());
-        a = vec![0; FIELD_LENGTH.try_into().unwrap()];
-        return a;
+#[derive(Debug)]
+pub struct Config {
+    pub field: Vec<i32>,
+    pub queue_field: Vec<i32>,
+    pub queue: Vec<i32>,
+    pub angle: i32,
+    pub current: store::statics::BlockPosition,
+    pub count: i32,
+    pub completed_row: i32,
+    pub speed: i32,
+    pub level: i32,
+    pub score: i32,
+    pub one_row_array: Vec<[i32; 2]>,
+    pub complete_row_numbers: Vec<i32>,
+    pub remain_row_array: Vec<[i32; 2]>,
+    pub remain_row_numbers: Vec<i32>,
+}
+
+fn default_field_value() -> Vec<i32> {
+    let mut f = Vec::with_capacity(FIELD_LENGTH.try_into().unwrap());
+    f = vec![store::statics::Number::EMPTY; FIELD_LENGTH.try_into().unwrap()];
+    return f;
+}
+
+fn default_queue_field_value() -> Vec<i32> {
+    let mut q = Vec::with_capacity(
+        (store::statics::Number::QUEUE_ROW * store::statics::Number::QUEUE_ROW)
+            .try_into()
+            .unwrap(),
+    );
+    q = vec![
+        store::statics::Number::EMPTY;
+        (store::statics::Number::QUEUE_ROW * store::statics::Number::QUEUE_ROW)
+            .try_into()
+            .unwrap()
+    ];
+    return q;
+}
+
+pub fn default_queue_value() -> Vec<i32> {
+    let mut queue: Vec<i32> = (0..store::statics::BLOCKS.len() as i32).collect();
+    let mut rng = thread_rng();
+    queue.shuffle(&mut rng);
+
+    return queue;
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        Self {
+            field: default_field_value(),
+            queue_field: default_queue_field_value(),
+            queue: default_queue_value(),
+            angle: 0,
+            current: [0, 0, 0, 0],
+            count: 1,
+            completed_row: 0,
+            speed: 1000,
+            level: 0,
+            score: 0,
+            one_row_array: vec![[0; 2]],
+            complete_row_numbers: vec![],
+            remain_row_array: vec![[0; 2]],
+            remain_row_numbers: vec![],
+        }
+    }
+}
+
+trait Update {
+    fn create_queue(&self) -> Vec<i32>;
+}
+
+impl Update for Config {
+    fn create_queue(&self) -> Vec<i32> {
+        let mut queue = self.queue.clone();
+
+        // delete first block
+        if self.count >= 1 {
+            queue.drain(0..1);
+        }
+        // pick random between especially vector length
+        if queue.len() > 0 && queue.len() < 4 {
+            fn random() -> i32 {
+                let mut rng = thread_rng();
+                let r: i32 = rng
+                    .gen_range(0, &store::statics::BLOCKS.len())
+                    .try_into()
+                    .unwrap();
+                return r;
+            }
+
+            while true {
+                let serve = random();
+
+                if !queue.contains(&serve) {
+                    queue.push(serve);
+                    break;
+                }
+            }
+        }
+        return queue;
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::store;
-    use super::Field;
-    use rand::{thread_rng, Rng};
+
+    use super::Config;
+
+    use super::Update;
 
     #[test]
     fn some_test() {
-        let mut field = Field::init();
-        field[0] = 999;
-        println!("{:?}", &field);
+        let options: Config = Default::default();
 
-        // let choices = [1, 2, 4, 8, 16, 32];
-        // let mut rng = thread_rng();
-        // println!("{:?}", rng.choose(&choices));
+        let queue = options.create_queue();
+
+        println!("{:?}", queue);
     }
 }
 
